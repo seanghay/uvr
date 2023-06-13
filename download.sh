1627e69dde0e8801b1a3e26e73fc561866a6e0c6
Seanghay Yath <seanghay.dev@gmail.com>
2023-06-13 20:23:51 +0700

    update download scrpit

--- download.sh
+++ download.sh
@@ -2,4 +2,6 @@
 
 if [ ! -f models.zip ]; then
   wget -O models.zip https://github.com/Anjok07/ultimatevocalremovergui/releases/download/v5.3.0/models.zip
+  unzip models.zip
+  cp models/Main_Models/1_HP-UVR.pth uvr5_weights/1_HP-UVR.pth
 fi
